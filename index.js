25423e060524108c7d19053acfe97be536224ef9
ome-janusz <ome-janusz@janusz.shop>
2020-11-11 09:35:24 +0100

    Adding keyword matching

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const auth = require("./auth.json");
 const config = require("./config.json");
 const urlRegex = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?")
 
+function isSpam(content) {
+    return urlRegex.test(content) || config.keywords.find(w => content.toLowerCase().indexOf(w) != -1) ? true : false;
+}
+
 client.on("ready", () => {
   console.log(`Bot has started as ${client.user.tag}`); 
 });
@@ -11,7 +15,7 @@ client.on("ready", () => {
 client.on("message", (message) => {
   if (message.member.id != client.user.id
     	&& Date.now() - message.member.joinedTimestamp < config.membershipThreshold
-    	&& urlRegex.test(message.content)) {
+    	&& isSpam(message.content)) {
     if (config.hasOwnProperty('notificationChannelId')) {
       client.channels.fetch(config.notificationChannelId)
         .then(channel => channel.send(`Linkspam geplaatst door gebruiker <@${message.member.id}>; gebruiker wordt gekickt.`));
